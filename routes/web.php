b77383a44cafa05dd025ff971cbdefb87cc52780
ramanandhaaja <thisisnandha@gmail.com>
2024-10-08 17:17:44 +0700

    add post

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('images', ImageController::class);
+
+Route::post('posts', [PostController::class]);
