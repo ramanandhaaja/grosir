a834f206ae72c5aeda32bd638bb3c4f6a6751618
ramanandhaaja <thisisnandha@gmail.com>
2024-10-09 04:15:06 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'https://dashgrosir.flutterflow.app'],
+    'allowed_origins' => ['*', 'https://dashgrosir.flutterflow.app', 'https://dashgrosir.flutterflow.app/teamPage'],
 
     'allowed_origins_patterns' => ['*'],
 
