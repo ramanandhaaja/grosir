5a08c75ee544487fa9e1d677fed064f24449ad44
ramanandhaaja <thisisnandha@gmail.com>
2024-10-08 17:24:16 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'posts/*',
+            '/posts',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
