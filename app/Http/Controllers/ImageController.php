872e6dffd532a0da4dfaeaf3c04e49d9b629f091
ramanandhaaja <thisisnandha@gmail.com>
2024-10-08 17:33:50 +0700

    Update ImageController.php

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -28,25 +28,68 @@ class ImageController extends Controller
      */
     public function store(Request $request)
     {
-        @csrf_token();
-        $imagePath = $request->ID;
-        echo $request->ID;
-        echo $request->name;
+        $img = imagecreatefromjpeg('./assets/invoice_format.jpg');
+        $color = imagecolorallocate($img, 0, 0, 0);
 
-        //return view('image',compact('imagePath'));
-        return 'POST REQUESTNYA BENER';
+        $font = "./assets/GOTHAM-MEDIUM.TTF";
 
-        /*
-        $customer = new Customer();
-        $customer->first_name = $request->first_name;
-        $customer->last_name = $request->last_name;
-        $customer->email = $request->email;
-        $customer->phone = $request->phone;
-        $customer->bank_account_number = $request->bank_account_number;
-        $customer->about = $request->about;
-        $customer->save();
+        $invoice_number = $request->ID;
+        $invoice_date = "11/9/2024";
 
-        return redirect()->route('customers.index');*/
+        //$name = $customer->first_name;
+        $name=$request->name;
+        $address = "Jalan Kemang Timur Raya no 62";
+
+        $new_name = "A.jpeg";
+
+        $product_name = " Minyak Kita 1L";
+        $product_qty = " 1";
+        $product_price = " 18.000";
+        $product_total = " 18.000";
+
+        $sub_total = "36.000";
+        $tax = "0";
+        $grand_total = "36.000";
+
+        //Invoice NUMBER
+        imagettftext($img, 20, 0, 1020, 380, $color, $font, $invoice_number);
+        imagettftext($img, 20, 0, 1020, 422, $color, $font, $invoice_date);
+
+        //Customer Detail
+        imagettftext($img, 20, 0, 142, 422, $color, $font, $name);
+        imagettftext($img, 20, 0, 142, 455, $color, $font, $address);
+
+        //Content
+        imagettftext($img, 24, 0, 132, 1000, $color, $font, $product_name);
+        imagettftext($img, 24, 0, 740, 1000, $color, $font, $product_qty);
+        imagettftext($img, 24, 0, 890, 1000, $color, $font, $product_price);
+        imagettftext($img, 24, 0, 1100, 1000, $color, $font, $product_total);
+
+        imagettftext($img, 24, 0, 132, 1050, $color, $font, $product_name);
+        imagettftext($img, 24, 0, 740, 1050, $color, $font, $product_qty);
+        imagettftext($img, 24, 0, 890, 1050, $color, $font, $product_price);
+        imagettftext($img, 24, 0, 1100, 1050, $color, $font, $product_total);
+
+        //Total
+        imagettftext($img, 24, 0, 1100, 1429, $color, $font, $sub_total);
+        imagettftext($img, 24, 0, 1100, 1490, $color, $font, $tax);
+        imagettftext($img, 24, 0, 1100, 1545, $color, $font, $grand_total);
+
+
+        /* OUTPUT IMAGE */
+        header('Content-type: image/jpeg');
+
+        /* create directory */
+        $directory = "./assets/uploads/certificates/user/";
+        $directoryJson = asset('assets/uploads/certificates/user/');
+
+
+        /* image save */
+        imagejpeg($img, $directory . $new_name);
+        $imagePath = $directoryJson ."/". $new_name;
+
+        //return redirect(asset($imagePath));
+        return view('image',compact('imagePath'));
     }
 
     /**
