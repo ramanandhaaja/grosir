8d06e1d746b3855c7b19185f2e8e5bec4f16a85c
ramanandhaaja <thisisnandha@gmail.com>
2024-10-09 10:47:31 +0700

    Update image.blade.php

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -1,12 +1,10 @@
-<html>
-<head>
+
 <?php
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 ?>
-</head>
+
 {"imagePath":"{{ asset($imagePath) }}"}
 nandha
-</html>
