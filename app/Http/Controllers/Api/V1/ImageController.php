700e533910346d3e47dae59ef44394d7cf30c0af
ramanandhaaja <thisisnandha@gmail.com>
2024-10-09 12:26:26 +0700

    add api

---
+++ app/Http/Controllers/Api/V1/ImageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    function index(){
+        /*
+        $post = Blog::all();
+        return response()->json($post);*/
+
+        return '{"image":"something.jpg"}';
+    }
+
+    function store(Request $request){
+        /*
+        $post = new Blog();
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->image = $request->image;
+        $post->author_id = $request->author_id;
+        $post->save();
+        return response()->json($post, 201);*/
+
+        return '{"image":"something post.jpg"}';
+    }
+
+}
